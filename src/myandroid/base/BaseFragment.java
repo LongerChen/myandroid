c9861d259426978e7a545c656a41d41354474342
Hiiir <Hiiir@idea-PC>
2014-02-25 17:53:42 +0800

    BaseFragment update

--- src/myandroid/base/BaseFragment.java
+++ src/myandroid/base/BaseFragment.java
@@ -1,6 +1,7 @@
 package myandroid.base;
 
 import android.app.Activity;
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -8,10 +9,12 @@ import android.view.View.OnClickListener;
 public abstract class BaseFragment<A extends Activity> extends Fragment
 		implements OnClickListener {
 	protected A activity;
+	Context context;
 
 	@Override
 	public void onAttach(Activity activity) {
 		this.activity = (A) activity;
+		this.context = activity;
 		super.onAttach(activity);
 	}
 
