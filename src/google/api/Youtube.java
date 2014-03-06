d5d3af8814f4410447654a4f49c379c627063043
Hiiir <Hiiir@idea-PC>
2014-03-06 14:58:02 +0800

    update

---
+++ src/google/api/Youtube.java
@@ -0,0 +1,11 @@
+package google.api;
+
+public class Youtube {
+	public final static String KEY = "AIzaSyCuYh5xABe5AMUspD-hm15wY-hPDjCQKwk";
+	public final static String DOMAIN = "https://www.googleapis.com/youtube/v3/";
+	public final static String CHANNELS = DOMAIN + "channels";
+	public final static String SEARCH = DOMAIN + "search";
+	public final static String PLAYLISTS = DOMAIN + "playlists";
+	public final static String PLAYLISTITEM = DOMAIN + "playlistItems";
+	public final static String VIDEOINFO = "http://www.youtube.com/get_video_info";
+}
---
+++ src/myandroid/tools/Package.java
@@ -0,0 +1,5 @@
+package myandroid.tools;
+
+public class Package {
+	final public static String GOOGLEPLAY = "com.android.vending";
+}
