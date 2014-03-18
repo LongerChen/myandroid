2afd69ae9f28eef2ec646d7a93a6c318a22b2a92
Hiiir <Hiiir@10.100.81.9>
2014-03-18 12:16:09 +0800

    Http update

--- src/myandroid/http/Http.java
+++ src/myandroid/http/Http.java
@@ -13,7 +13,7 @@ import myandroid.tools.Develop;
 public class Http {
 	static HttpURLConnection connect;
 
-	public Http url(String url) {
+	public Http(String url) {
 		try {
 			connect = (HttpURLConnection) new URL(url).openConnection();
 		} catch (MalformedURLException e) {
@@ -21,7 +21,6 @@ public class Http {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		return Http.this;
 	}
 
 	public Http header(Map<String, String> headers) {
@@ -45,13 +44,13 @@ public class Http {
 		return this;
 	}
 
-	public HttpURLConnection body(String body) {
+	public Http body(String body) {
 		if (connect == null) {
 			Develop.e(this, "Call url() first");
-			return connect;
+			return this;
 		}
 		if (connect.getRequestMethod().equals(HttpMethod.GET))
-			return connect;
+			return this;
 
 		connect.setDoOutput(true);
 		try {
@@ -66,79 +65,11 @@ public class Http {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		return this;
+	}
+
+	public HttpURLConnection create() {
 		return connect;
-	}
-
-	public static HttpURLConnection connect(String url) {
-		return create(url, HttpMethod.CONNECT);
-	}
-
-	public static HttpURLConnection delete(String url) {
-		return create(url, HttpMethod.DELETE);
-	}
-
-	public static HttpURLConnection get(String url) {
-		return create(url, HttpMethod.GET);
-	}
-
-	public static HttpURLConnection head(String url) {
-		return create(url, HttpMethod.HEAD);
-	}
-
-	public static HttpURLConnection options(String url) {
-		return create(url, HttpMethod.OPTIONS);
-	}
-
-	public static HttpURLConnection post(String url) {
-		return create(url, HttpMethod.POST);
-	}
-
-	public static HttpURLConnection post(String url, String body) {
-		return create(url, HttpMethod.POST, body);
-	}
-
-	public static HttpURLConnection put(String url) {
-		return create(url, HttpMethod.PUT);
-	}
-
-	public static HttpURLConnection put(String url, String body) {
-		return create(url, HttpMethod.PUT, body);
-	}
-
-	public static HttpURLConnection trace(String url) {
-		return create(url, HttpMethod.TRACE);
-	}
-
-	private static HttpURLConnection create(String url, String httpmethod) {
-		return create(url, httpmethod, null);
-	}
-
-	private static HttpURLConnection create(String url, String httpmethod,
-			String body) {
-		HttpURLConnection connection = null;
-		try {
-			connection = (HttpURLConnection) new URL(url).openConnection();
-			connection.setRequestMethod(httpmethod);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		if (httpmethod != HttpMethod.GET)
-			connection.setDoOutput(true);
-		if (body == null || connection == null)
-			return connection;
-		try {
-			DataOutputStream out = new DataOutputStream(
-					connection.getOutputStream());
-
-			byte[] data = body.getBytes();
-			out.write(data, 0, data.length);
-			out.flush();
-			out.close();
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return connection;
 	}
 
 }
